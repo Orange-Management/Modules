1eab9da54c46f65626d2bbe5833dee9f5919de80
Dennis Eichhorn <spl1nes.com@googlemail.com>
2016-06-11 20:33:43 +0200

    Comment added

--- Admin/Models/Account.php
+++ Admin/Models/Account.php
@@ -15,6 +15,17 @@
  */
 namespace Modules\Admin\Models;
 
+/**
+ * Account class.
+ *
+ * @category   Modules
+ * @package    Modules\Admin
+ * @author     OMS Development Team <dev@oms.com>
+ * @author     Dennis Eichhorn <d.eichhorn@oms.com>
+ * @license    OMS License 1.0
+ * @link       http://orange-management.com
+ * @since      1.0.0
+ */
 class Account extends \phpOMS\Account\Account
 {
 }
