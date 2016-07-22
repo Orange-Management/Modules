561fd1f8355d86835caa0247045e3389fcde0406
Dennis Eichhorn <spl1nes.com@googlemail.com>
2016-07-22 20:24:51 +0200

    Path fix

---
+++ Dashboard/Theme/Backend/dashboard.php
@@ -0,0 +1,4 @@
+<?php include __DIR__ . '/../../../News/Theme/Backend/dashbaord-news.tpl.php' ;?> 
+<?php include __DIR__ . '/../../../Calendar/Theme/Backend/dashboard-calendar.tpl.php' ;?> 
+<?php include __DIR__ . '/../../../Tasks/Theme/Backend/dashboard-task.tpl.php' ;?> 
+<?php include __DIR__ . '/../../../Reporter/Theme/Backend/dashboard-report.tpl.php' ;?> 
