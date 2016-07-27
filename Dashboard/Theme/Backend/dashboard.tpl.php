d8b13f4a46c3500fac7e4c15d579c068d7ba658b
Dennis Eichhorn <spl1nes.com@googlemail.com>
2016-07-27 10:52:11 +0200

    Database draft

--- Dashboard/Theme/Backend/dashboard.tpl.php
+++ Dashboard/Theme/Backend/dashboard.tpl.php
@@ -1,5 +1,4 @@
-<?php include __DIR__ . '/../../../News/Theme/Backend/dashboard-news.tpl.php' ;?> 
-<?php include __DIR__ . '/../../../Calendar/Theme/Backend/dashboard-calendar.tpl.php' ;?> 
-<?php include __DIR__ . '/../../../Tasks/Theme/Backend/dashboard-task.tpl.php' ;?> 
-<?php include __DIR__ . '/../../../Reporter/Theme/Backend/dashboard-reporter.tpl.php' ;?> 
-daten
+<?php include __DIR__ . '/../../../News/Theme/Backend/dashboard-news.tpl.php'; ?> 
+<?php include __DIR__ . '/../../../Calendar/Theme/Backend/dashboard-calendar.tpl.php'; ?> 
+<?php include __DIR__ . '/../../../Tasks/Theme/Backend/dashboard-task.tpl.php'; ?> 
+<?php include __DIR__ . '/../../../Reporter/Theme/Backend/dashboard-reporter.tpl.php'; ?> 
---
+++ Database/Controller.php
@@ -0,0 +1,138 @@
+<?php
+/**
+ * Orange Management
+ *
+ * PHP Version 7.0
+ *
+ * @category   TBD
+ * @package    TBD
+ * @author     OMS Development Team <dev@oms.com>
+ * @author     Dennis Eichhorn <d.eichhorn@oms.com>
+ * @copyright  2013 Dennis Eichhorn
+ * @license    OMS License 1.0
+ * @version    1.0.0
+ * @link       http://orange-management.com
+ */
+namespace Modules\Database;
+
+use phpOMS\Asset\AssetType;
+use phpOMS\Message\RequestAbstract;
+use phpOMS\Message\ResponseAbstract;
+use phpOMS\Model\Html\Head;
+use phpOMS\Module\ModuleAbstract;
+use phpOMS\Module\WebInterface;
+use phpOMS\Views\View;
+
+/**
+ * Database controller class.
+ *
+ * @category   Modules
+ * @package    Database
+ * @author     OMS Development Team <dev@oms.com>
+ * @author     Dennis Eichhorn <d.eichhorn@oms.com>
+ * @license    OMS License 1.0
+ * @link       http://orange-management.com
+ * @since      1.0.0
+ */
+class Controller extends ModuleAbstract implements WebInterface
+{
+
+    /**
+     * Module path.
+     *
+     * @var string
+     * @since 1.0.0
+     */
+    const MODULE_PATH = __DIR__;
+
+    /**
+     * Module version.
+     *
+     * @var string
+     * @since 1.0.0
+     */
+    const MODULE_VERSION = '1.0.0';
+
+    /**
+     * Module name.
+     *
+     * @var string
+     * @since 1.0.0
+     */
+    const MODULE_NAME = 'Database';
+
+    /**
+     * Providing.
+     *
+     * @var string
+     * @since 1.0.0
+     */
+    protected static $providing = [];
+
+    /**
+     * Dependencies.
+     *
+     * @var string
+     * @since 1.0.0
+     */
+    protected static $dependencies = [
+    ];
+
+    /**
+     * @param RequestAbstract  $request  Request
+     * @param ResponseAbstract $response Response
+     * @param mixed            $data     Generic data
+     *
+     * @return \Serializable
+     *
+     * @since  1.0.0
+     * @author Dennis Eichhorn <d.eichhorn@oms.com>
+     */
+    public function viewDatabaseList(RequestAbstract $request, ResponseAbstract $response, $data = null) : \Serializable
+    {
+        $view = new View($this->app, $request, $response);
+        $view->setTemplate('/Modules/Database/Theme/Backend/database-list');
+        $view->addData('nav', $this->app->moduleManager->get('Navigation')->createNavigationMid(1005201001, $request, $response));
+
+        return $view;
+    }
+
+    /**
+     * @param RequestAbstract  $request  Request
+     * @param ResponseAbstract $response Response
+     * @param mixed            $data     Generic data
+     *
+     * @return \Serializable
+     *
+     * @since  1.0.0
+     * @author Dennis Eichhorn <d.eichhorn@oms.com>
+     */
+    public function viewDatabaseCreate(RequestAbstract $request, ResponseAbstract $response, $data = null) : \Serializable
+    {
+        $view = new View($this->app, $request, $response);
+        $view->setTemplate('/Modules/Database/Theme/Backend/database-generator');
+        $view->addData('nav', $this->app->moduleManager->get('Navigation')->createNavigationMid(1005201001, $request, $response));
+
+        return $view;
+    }
+
+    /**
+     * @param RequestAbstract  $request  Request
+     * @param ResponseAbstract $response Response
+     * @param mixed            $data     Generic data
+     *
+     * @return \Serializable
+     *
+     * @since  1.0.0
+     * @author Dennis Eichhorn <d.eichhorn@oms.com>
+     */
+    public function viewDatabaseResult(RequestAbstract $request, ResponseAbstract $response, $data = null) : \Serializable
+    {
+        $view = new View($this->app, $request, $response);
+        $view->setTemplate('/Modules/Database/Theme/Backend/database-query-result');
+        $view->addData('nav', $this->app->moduleManager->get('Navigation')->createNavigationMid(1005201001, $request, $response));
+
+        return $view;
+    }
+
+}
