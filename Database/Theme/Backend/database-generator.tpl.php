d4a4179197167ba544fe27fead03f9b7cc47d40c
Dennis Eichhorn <spl1nes.com@googlemail.com>
2016-08-02 15:36:21 +0200

    Database draft
    Need "timeline" like tabs for query builder.

---
+++ Database/Theme/Backend/database-generator.tpl.php
@@ -0,0 +1,88 @@
+<?php
+/**
+ * Orange Management
+ *
+ * PHP Version 7.0
+ *
+ * @category   TBD
+ * @package    TBD
+ * @author     OMS Development Team <dev@oms.com>
+ * @author     Dennis Eichhorn <d.eichhorn@oms.com>
+ * @copyright  2013 Dennis Eichhorn
+ * @license    OMS License 1.0
+ * @version    1.0.0
+ * @link       http://orange-management.com
+ */
+echo $this->getData('nav')->render(); ?>
+
+<section class="box w-33 floatLeft">
+    <header><h1><?= $this->getText('Query'); ?></h1></header>
+    <div class="inner">
+        <form>
+            <table class="layout w-100">
+                <tr><td><label for="iStatus"><?= $this->getText('Status') ?></label>
+                <tr><td colspan="2"><input type="submit" value="<?= $this->getText('Next'); ?>">
+            </table>
+        </form>
+    </div>
+</section>
+
+<section class="box w-33 floatLeft">
+    <header><h1><?= $this->getText('Fields'); ?></h1></header>
+    <div class="inner">
+        <form>
+            <table class="layout w-100">
+                <tr><td><label for="iStatus"><?= $this->getText('Status') ?></label>
+                <tr><td colspan="2"><input type="submit" value="<?= $this->getText('Next'); ?>">
+            </table>
+        </form>
+    </div>
+</section>
+
+<section class="box w-33 floatLeft">
+    <header><h1><?= $this->getText('Action'); ?></h1></header>
+    <div class="inner">
+        <form>
+            <table class="layout w-100">
+                <tr><td><label for="iStatus"><?= $this->getText('Status') ?></label>
+                <tr><td colspan="2"><input type="submit" value="<?= $this->getText('Next'); ?>">
+            </table>
+        </form>
+    </div>
+</section>
+
+<section class="box w-33 floatLeft">
+    <header><h1><?= $this->getText('Interval'); ?></h1></header>
+    <div class="inner">
+        <form>
+            <table class="layout w-100">
+                <tr><td><label for="iStatus"><?= $this->getText('Status') ?></label>
+                <tr><td colspan="2"><input type="submit" value="<?= $this->getText('Next'); ?>">
+            </table>
+        </form>
+    </div>
+</section>
+
+<section class="box w-33 floatLeft">
+    <header><h1><?= $this->getText('Options'); ?></h1></header>
+    <div class="inner">
+        <form>
+            <table class="layout w-100">
+                <tr><td><label for="iStatus"><?= $this->getText('Status') ?></label>
+                <tr><td colspan="2"><input type="submit" value="<?= $this->getText('Next'); ?>">
+            </table>
+        </form>
+    </div>
+</section>
+
+<section class="box w-33 floatLeft">
+    <header><h1><?= $this->getText('Layout'); ?></h1></header>
+    <div class="inner">
+        <form>
+            <table class="layout w-100">
+                <tr><td><label for="iStatus"><?= $this->getText('Status') ?></label>
+                <tr><td colspan="2"><input type="submit" value="<?= $this->getText('Next'); ?>">
+            </table>
+        </form>
+    </div>
+</section>
---
+++ Database/Theme/Backend/database-list.tpl.php
@@ -0,0 +1,50 @@
+<?php
+/**
+ * Orange Management
+ *
+ * PHP Version 7.0
+ *
+ * @category   TBD
+ * @package    TBD
+ * @author     OMS Development Team <dev@oms.com>
+ * @author     Dennis Eichhorn <d.eichhorn@oms.com>
+ * @copyright  2013 Dennis Eichhorn
+ * @license    OMS License 1.0
+ * @version    1.0.0
+ * @link       http://orange-management.com
+ */
+/**
+ * @var \phpOMS\Views\View $this
+ */
+
+$queries      = [];
+$footerView = new \Web\Views\Lists\PaginationView($this->app, $this->request, $this->response);
+$footerView->setTemplate('/Web/Templates/Lists/Footer/PaginationBig');
+$footerView->setPages(count($queries) / 25);
+$footerView->setPage(1);
+
+echo $this->getData('nav')->render(); ?>
+<div class="box">
+    <table class="table">
+        <caption><?= $this->getText('Templates'); ?></caption>
+        <thead>
+        <tr>
+            <td class="wf-100"><?= $this->getText('Name'); ?>
+            <td><?= $this->getText('Creator'); ?>
+            <td><?= $this->getText('Created'); ?>
+                <tfoot>
+        <tr>
+            <td colspan="3"><?= $footerView->render(); ?>
+                <tbody>
+                <?php $count = 0; foreach($queries as $key => $value) : $count++;
+                $url = \phpOMS\Uri\UriFactory::build('/{/lang}/backend/database/template/single?id=' . $value->getId()); ?>
+                <tr>
+                    <td><a href="<?= $url; ?>"><?= $value->getName(); ?></a>
+                    <td><a href="<?= $url; ?>"><?= $value->getCreatedBy(); ?></a>
+                    <td><a href="<?= $url; ?>"><?= $value->getCreatedAt()->format('Y-m-d H:i:s'); ?></a>
+                <?php endforeach; ?>
+                <?php if($count === 0) : ?>
+        <tr><td colspan="5" class="empty"><?= $this->getText('Empty', 0, 0); ?>
+                <?php endif; ?>
+    </table>
+</div>
