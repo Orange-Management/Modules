2f726261a64cbe766594408cee931ad1b54fa3da
Dennis Eichhorn <spl1nes.com@googlemail.com>
2016-08-13 14:13:07 +0200

    Test fixes

---
+++ AreaManager/Controller.php
@@ -0,0 +1,75 @@
+<?php
+/**
+ * Orange Management
+ *
+ * PHP Version 7.0
+ *
+ * @category   TBD
+ * @package    TBD
+ * @author     OMS Development Team <dev@oms.com>
+ * @author     Dennis Eichhorn <d.eichhorn@oms.com>
+ * @copyright  2013 Dennis Eichhorn
+ * @license    OMS License 1.0
+ * @version    1.0.0
+ * @link       http://orange-management.com
+ */
+namespace Modules\AreaManager;
+
+use phpOMS\Module\ModuleAbstract;
+use phpOMS\Module\WebInterface;
+
+/**
+ * AreaManager controller class.
+ *
+ * @category   Modules
+ * @package    Modules\Arrival
+ * @author     OMS Development Team <dev@oms.com>
+ * @author     Dennis Eichhorn <d.eichhorn@oms.com>
+ * @license    OMS License 1.0
+ * @link       http://orange-management.com
+ * @since      1.0.0
+ */
+class Controller extends ModuleAbstract implements WebInterface
+{
+
+    /**
+     * Module path.
+     *
+     * @var string
+     * @since 1.0.0
+     */
+    const MODULE_PATH = __DIR__;
+
+    /**
+     * Module version.
+     *
+     * @var string
+     * @since 1.0.0
+     */
+    const MODULE_VERSION = '1.0.0';
+
+    /**
+     * Module name.
+     *
+     * @var string
+     * @since 1.0.0
+     */
+    const MODULE_NAME = 'AreaManager';
+
+    /**
+     * Providing.
+     *
+     * @var string
+     * @since 1.0.0
+     */
+    protected static $providing = [];
+
+    /**
+     * Dependencies.
+     *
+     * @var string
+     * @since 1.0.0
+     */
+    protected static $dependencies = [
+    ];
+}
