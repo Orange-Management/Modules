b8cae5c0cd3dc12fef2285c605058697013b946b
Dennis Eichhorn <spl1nes.com@googlemail.com>
2017-01-29 13:43:20 +0100

    Flexbox tpl implementation

--- Tasks/Theme/backend/task-analysis.tpl.php
+++ Tasks/Theme/backend/task-analysis.tpl.php
@@ -18,41 +18,49 @@
  */
 echo $this->getData('nav')->render(); ?>
 
-<section class="box w-50 floatLeft">
-    <header><h1><?= $this->getText('Account'); ?></h1></header>
-    <div class="inner">
-        <form>
-            <table class="layout wf-100">
-                <tr><td><label for="iAccount"><?= $this->getText('Account'); ?></label>
-                <tr><td><span class="input"><button type="button" formaction=""><i class="fa fa-book"></i></button><input type="number" min="1" id="iAccount" name="account" placeholder="&#xf007; Guest" required></span>
-                <tr><td><label for="iFrom"><?= $this->getText('From'); ?></label>
-                <tr><td><input type="datetime-local" id="iFrom" name="from" value="<?= (new \DateTime('NOW'))->format('Y-m-d\TH:i:s') ?>">
-                <tr><td><label for="iTo"><?= $this->getText('To'); ?></label>
-                <tr><td><input type="datetime-local" id="iTo" name="to" value="<?= (new \DateTime('NOW'))->format('Y-m-d\TH:i:s') ?>">
-                <tr><td><input type="submit" value="<?= $this->getText('Submit', 0, 0); ?>">
-            </table>
-        </form>
+<div class="row">
+    <div class="col-xs-12 col-md-6">
+        <section class="box wf-100">
+            <header><h1><?= $this->getText('Account'); ?></h1></header>
+            <div class="inner">
+                <form>
+                    <table class="layout wf-100">
+                        <tr><td><label for="iAccount"><?= $this->getText('Account'); ?></label>
+                        <tr><td><span class="input"><button type="button" formaction=""><i class="fa fa-book"></i></button><input type="number" min="1" id="iAccount" name="account" placeholder="&#xf007; Guest" required></span>
+                        <tr><td><label for="iFrom"><?= $this->getText('From'); ?></label>
+                        <tr><td><input type="datetime-local" id="iFrom" name="from" value="<?= (new \DateTime('NOW'))->format('Y-m-d\TH:i:s') ?>">
+                        <tr><td><label for="iTo"><?= $this->getText('To'); ?></label>
+                        <tr><td><input type="datetime-local" id="iTo" name="to" value="<?= (new \DateTime('NOW'))->format('Y-m-d\TH:i:s') ?>">
+                        <tr><td><input type="submit" value="<?= $this->getText('Submit', 0, 0); ?>">
+                    </table>
+                </form>
+            </div>
+        </section>
     </div>
-</section>
 
-<section class="box w-50 floatLeft">
-    <header><h1><?= $this->getText('Statistics'); ?></h1></header>
-    <div class="inner">
-        <table class="list wf-100">
-            <tr><td><?= $this->getText('Received'); ?><td>0
-            <tr><td><?= $this->getText('Created'); ?><td>0
-            <tr><td><?= $this->getText('Forwarded'); ?><td>0
-            <tr><td><?= $this->getText('AverageAmount'); ?><td>0
-            <tr><td><?= $this->getText('AverageProcessTime'); ?><td>0
-            <tr><td><?= $this->getText('InTime'); ?><td>0
-        </table>
+    <div class="col-xs-12 col-md-6">
+        <section class="box wf-100">
+            <header><h1><?= $this->getText('Statistics'); ?></h1></header>
+            <div class="inner">
+                <table class="list wf-100">
+                    <tr><td><?= $this->getText('Received'); ?><td>0
+                    <tr><td><?= $this->getText('Created'); ?><td>0
+                    <tr><td><?= $this->getText('Forwarded'); ?><td>0
+                    <tr><td><?= $this->getText('AverageAmount'); ?><td>0
+                    <tr><td><?= $this->getText('AverageProcessTime'); ?><td>0
+                    <tr><td><?= $this->getText('InTime'); ?><td>0
+                </table>
+            </div>
+        </section>
     </div>
-</section>
+</div>
 
-<div class="clear"></div>
-
-<section class="box w-100 floatLeft">
-    <header><h1><?= $this->getText('History'); ?></h1></header>
-    <div class="inner" style="height: 300px">
+<div class="row">
+    <div class="col-xs-12">
+        <section class="box wf-100">
+            <header><h1><?= $this->getText('History'); ?></h1></header>
+            <div class="inner" style="height: 300px">
+            </div>
+        </section>
     </div>
-</section>
+</div>
