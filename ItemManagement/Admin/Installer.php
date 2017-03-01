c54984a5ce24553f4f0cacfd45b0042dec88302d
Dennis Eichhorn <spl1nes.com@googlemail.com>
2017-03-01 16:47:36 +0100

    Preparing db

--- ItemManagement/Admin/Installer.php
+++ ItemManagement/Admin/Installer.php
@@ -45,13 +45,97 @@ class Installer extends InstallerAbstract
         switch ($dbPool->get('core')->getType()) {
             case DatabaseType::MYSQL:
                 $dbPool->get('core')->con->prepare(
-                    'CREATE TABLE if NOT EXISTS `' . $dbPool->get('core')->prefix . 'itemreference` (
-                            `itemreference_id` int(11) NOT NULL AUTO_INCREMENT,
-                            `itemreference_name` varchar(30) DEFAULT NULL,
-                            `itemreference_desc` varchar(256) DEFAULT NULL,
-                            PRIMARY KEY (`itemreference_id`)
+                    'CREATE TABLE if NOT EXISTS `' . $dbPool->get('core')->prefix . 'itemreference_item` (
+                            `itemreference_item_id` int(11) NOT NULL AUTO_INCREMENT,
+                            `itemreference_item_no` varchar(30) DEFAULT NULL,
+                            `itemreference_item_articlegroup` int(11) DEFAULT NULL,
+                            `itemreference_item_salesgroup` int(11) DEFAULT NULL,
+                            `itemreference_item_productgroup` int(11) DEFAULT NULL,
+                            `itemreference_item_segment` int(11) DEFAULT NULL,
+                            `itemreference_item_successor` int(11) DEFAULT NULL,
+                            PRIMARY KEY (`itemreference_item_id`)
                         )ENGINE=InnoDB DEFAULT CHARSET=utf8 AUTO_INCREMENT=1;'
                 )->execute();
+
+                $dbPool->get('core')->con->prepare(
+                    'CREATE TABLE if NOT EXISTS `' . $dbPool->get('core')->prefix . 'itemreference_media` (
+                            `itemreference_media_id` int(11) NOT NULL AUTO_INCREMENT,
+                            `itemreference_media_item` int(11) DEFAULT NULL,
+                            `itemreference_media_media` int(11) DEFAULT NULL,
+                            PRIMARY KEY (`itemreference_partslist_id`)
+                        )ENGINE=InnoDB DEFAULT CHARSET=utf8 AUTO_INCREMENT=1;'
+                )->execute();
+
+                $dbPool->get('core')->con->prepare(
+                    'CREATE TABLE if NOT EXISTS `' . $dbPool->get('core')->prefix . 'itemreference_docs` (
+                            `itemreference_docs_id` int(11) NOT NULL AUTO_INCREMENT,
+                            `itemreference_docs_item` int(11) DEFAULT NULL,
+                            `itemreference_docs_media` int(11) DEFAULT NULL,
+                            PRIMARY KEY (`itemreference_partslist_id`)
+                        )ENGINE=InnoDB DEFAULT CHARSET=utf8 AUTO_INCREMENT=1;'
+                )->execute();
+
+                $dbPool->get('core')->con->prepare(
+                    'CREATE TABLE if NOT EXISTS `' . $dbPool->get('core')->prefix . 'itemreference_localization` (
+                            `itemreference_localization_id` int(11) NOT NULL AUTO_INCREMENT,
+                            `itemreference_localization_language` varchar(30) DEFAULT NULL,
+                            `itemreference_localization_name1` varchar(30) DEFAULT NULL,
+                            `itemreference_localization_name2` varchar(30) DEFAULT NULL,
+                            `itemreference_localization_name3` varchar(30) DEFAULT NULL,
+                            `itemreference_localization_desc` text DEFAULT NULL,
+                            `itemreference_localization_item` text DEFAULT NULL,
+                            PRIMARY KEY (`itemreference_localization_id`)
+                        )ENGINE=InnoDB DEFAULT CHARSET=utf8 AUTO_INCREMENT=1;'
+                )->execute();
+
+                $dbPool->get('core')->con->prepare(
+                    'CREATE TABLE if NOT EXISTS `' . $dbPool->get('core')->prefix . 'itemreference_partslist` (
+                            `itemreference_partslist_id` int(11) NOT NULL AUTO_INCREMENT,
+                            `itemreference_partslist_item` int(11) DEFAULT NULL,
+                            `itemreference_partslist_ref` int(11) DEFAULT NULL,
+                            `itemreference_partslist_pos` int(11) DEFAULT NULL,
+                            `itemreference_partslist_amount` int(11) DEFAULT NULL,
+                            PRIMARY KEY (`itemreference_partslist_id`)
+                        )ENGINE=InnoDB DEFAULT CHARSET=utf8 AUTO_INCREMENT=1;'
+                )->execute();
+
+                $dbPool->get('core')->con->prepare(
+                    'CREATE TABLE if NOT EXISTS `' . $dbPool->get('core')->prefix . 'itemreference_purchase` (
+                            `itemreference_purchase_id` int(11) NOT NULL AUTO_INCREMENT,
+                            `itemreference_purchase_item` int(11) DEFAULT NULL,
+                            `itemreference_purchase_supplier` int(11) DEFAULT NULL,
+                            `itemreference_purchase_order_no` varchar(50) DEFAULT NULL,
+                            `itemreference_purchase_amount` int(11) DEFAULT NULL,
+                            `itemreference_purchase_leadtime` int(11) DEFAULT NULL,
+                            PRIMARY KEY (`itemreference_partslist_id`)
+                        )ENGINE=InnoDB DEFAULT CHARSET=utf8 AUTO_INCREMENT=1;'
+                )->execute();
+
+                $dbPool->get('core')->con->prepare(
+                    'CREATE TABLE if NOT EXISTS `' . $dbPool->get('core')->prefix . 'itemreference_purchase_price` (
+                            `itemreference_purchase_price_id` int(11) NOT NULL AUTO_INCREMENT,
+                            `itemreference_purchase_price_supplier` int(11) DEFAULT NULL,
+                            `itemreference_purchase_price_amount` varchar(50) DEFAULT NULL,
+                            `itemreference_purchase_price_price` int(11) DEFAULT NULL,
+                            `itemreference_purchase_price_bonus` int(11) DEFAULT NULL,
+                            `itemreference_purchase_price_discountp` int(11) DEFAULT NULL,
+                            `itemreference_purchase_price_discount` int(11) DEFAULT NULL,
+                            `itemreference_purchase_price_weight` int(11) DEFAULT NULL,
+                            PRIMARY KEY (`itemreference_purchase_price_id`)
+                        )ENGINE=InnoDB DEFAULT CHARSET=utf8 AUTO_INCREMENT=1;'
+                )->execute();
+
+				$dbPool->get('core')->con->prepare(
+                    'CREATE TABLE if NOT EXISTS `' . $dbPool->get('core')->prefix . 'itemreference_disposal` (
+                            `itemreference_disposal_id` int(11) NOT NULL AUTO_INCREMENT,
+                            `itemreference_disposal_item` int(11) DEFAULT NULL,
+                            `itemreference_disposal_type` int(11) DEFAULT NULL,
+                            `itemreference_disposal_amount` int(11) DEFAULT NULL,
+                            `itemreference_disposal_cost` int(11) DEFAULT NULL,
+                            PRIMARY KEY (`itemreference_disposal_id`)
+                        )ENGINE=InnoDB DEFAULT CHARSET=utf8 AUTO_INCREMENT=1;'
+                )->execute();
+
                 break;
         }
     }
