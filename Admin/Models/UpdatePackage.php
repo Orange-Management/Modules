64ad02cf1ae38feb2d6fdf14d908f97152f8c711
Dennis Eichhorn <spl1nes.com@googlemail.com>
2017-07-11 22:21:20 +0200

    Fix templates

---
+++ Admin/Models/UpdatePackage.php
@@ -0,0 +1,45 @@
+<?php
+/**
+ * Orange Management
+ *
+ * PHP Version 7.1
+ *
+ * @category   TBD
+ * @package    TBD
+ * @author     OMS Development Team <dev@oms.com>
+ * @author     Dennis Eichhorn <d.eichhorn@oms.com>
+ * @copyright  Dennis Eichhorn
+ * @license    OMS License 1.0
+ * @version    1.0.0
+ * @link       http://orange-management.com
+ */
+declare(strict_types=1);
+namespace Modules\Admin\Models;
+
+class UpdatePackage
+{
+    public function __construct(string $src, string $dest)
+    {
+
+    }
+
+    protected function unpack()
+    {
+
+    }
+
+    protected function validated()
+    {
+
+    }
+
+    private function validateSignature()
+    {
+
+    }
+
+    private function validateVersion()
+    {
+
+    }
+}
