a66097cdf4a7fe13beae79cd160fd3716091dbca
Dennis Eichhorn <spl1nes.com@googlemail.com>
2017-07-16 20:37:15 +0200

    Unique ID for dnd

--- Kanban/Theme/Backend/kanban-board.tpl.php
+++ Kanban/Theme/Backend/kanban-board.tpl.php
@@ -4,12 +4,12 @@ $columns = $board->getColumns();
 // todo: column width should be % but with min-width and on small screens full width
 ?>
 <div class="row">
-    <?php foreach($columns as $column) : $cards = $column->getCards(); ?>
-    <div class="col-xs-12 col-sm-3" draggable="true">
+    <?php $i = 0; foreach($columns as $column) : $i++; $cards = $column->getCards(); ?>
+    <div id="kanban-column-<?= $i; ?>" class="col-xs-12 col-sm-3" draggable="true">
         <header><?= $column->getName(); ?></header>
-        <?php foreach($cards as $card) : $labels = $card->getLabels(); ?>
+        <?php $j = 0; foreach($cards as $card) : $j++; $labels = $card->getLabels(); ?>
             <a href="<?= \phpOMS\Uri\UriFactory::build('{/base}/{/lang}/backend/kanban/card?{?}&id=' . $card->getId()) ?>">
-            <section class="box wf-100" draggable="true">
+            <section id="kanban-card-<?= $i . '-' . $j; ?>" class="box wf-100" draggable="true">
                 <header><h1><?= $card->getName(); ?></h1></header>
                 <div class="inner">
                     <?= $card->getDescription(); ?>
