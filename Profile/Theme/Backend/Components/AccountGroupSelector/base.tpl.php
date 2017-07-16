7a78c8c3b3a2c5467d249b1f40984ea1b9877dba
Dennis Eichhorn <spl1nes.com@googlemail.com>
2017-07-16 20:38:07 +0200

    Ne action draft

--- Profile/Theme/Backend/Components/AccountGroupSelector/base.tpl.php
+++ Profile/Theme/Backend/Components/AccountGroupSelector/base.tpl.php
@@ -12,32 +12,24 @@
 ]' formaction=""><i class="fa fa-book"></i></button>
 <input type="text" list="<?= $this->getId(); ?>-datalist" id="<?= $this->getId(); ?>" name="receiver" placeholder="&#xf007; Guest" data-action='[
     {
-        "listener": "keyup", "action": [
-            {"key": 1, "type": "utils.timer", "id": "<?= $this->getId(); ?>", "delay": 500, "resets": true},
-            {"key": 2, "type": "dom.datalist.clear", "id": "<?= $this->getId(); ?>-datalist"},
-            {"key": 3, "type": "message.request", "uri": "{/base}/{/lang}/api/admin/find/account?search={#<?= $this->getId(); ?>}", "method": "GET", "request_type": "json"},
-            {"key": 4, "type": "dom.datalist.append", "id": "<?= $this->getId(); ?>-datalist", "value": "id", "text": "name"}
-        ]
-    }
-]' required>
-<datalist id="<?= $this->getId(); ?>-datalist"></datalist>
-<input type="hidden" id="<?= $this->getId(); ?>-list"></span>
-<input data-action='[
-    {
-        "listener": "keyup", "action": [
-            {"key": 1, "type": "utils.timer", "id": "<?= $this->getId(); ?>", "delay": 500, "resets": true},
-            {"key": 2, "type": "dom.datalist.clear", "id": "<?= $this->getId(); ?>-datalist"},
-            {"key": 2, "type": "dom.class.add", "id": "<?= $this->getId(); ?>-datalist", "class": "hidden"},
-            {"key": 3, "type": "utils.validate", "id": "<?= $this->getId(); ?>-datalist", "pattern": ""},
+        "key": 1, "listener": "keyup", "action": [
+            {"key": 1, "type": "validate.keypress", "pressed": "!enter"},
+            {"key": 2, "type": "utils.timer", "id": "<?= $this->getId(); ?>", "delay": 500, "resets": true},
+            {"key": 3, "type": "dom.datalist.clear", "id": "<?= $this->getId(); ?>-datalist"},
             {"key": 4, "type": "message.request", "uri": "{/base}/{/lang}/api/admin/find/account?search={#<?= $this->getId(); ?>}", "method": "GET", "request_type": "json"},
-            {"key": 5, "type": "dom.set", "id": "", "data": ""},
-            {"key": 6, "type": "dom.add", "id": "asfdsaf", "data": [], "schema": "<li data-value=\"{$value}\">{$text}", "position": "end"},
-            {"key": 7, "type": "dom.unbind", "id": "asfdsaf"},
-            {"key": 8, "type": "dom.bind", "id": "asfdsaf"},
-            {"key": 2, "type": "dom.class.remove", "id": "<?= $this->getId(); ?>-datalist", "class": "hidden"}
+            {"key": 5, "type": "dom.datalist.append", "id": "<?= $this->getId(); ?>-datalist", "value": "id", "text": "name"}
+        ]
+    },
+    {
+        "key": 2, "listener": "keyup", "action" : [
+            {"key": 1, "type": "validate.keypress", "pressed": "enter"},
+            {"key": 2, "type": "message.request", "uri": "{/base}/{/lang}/api/admin/find/account?search={#<?= $this->getId(); ?>}", "method": "GET", "request_type": "json"},
+            {"key": 3, "type": "dom.set", "id": "<?= $this->getId(); ?>-idlist" "data": ""}
         ]
     }
-]'>
+]' required><!-- todo: handle keyup-enter -->
+<datalist id="<?= $this->getId(); ?>-datalist"></datalist>
+<input type="text" id="<?= $this->getId(); ?>-list"></span>
 <!--
 <ul class="select" data-action='[
     {
