9ece67359f60dbb2df14d9f89f8ecbdb1d871808
Dennis Eichhorn <spl1nes.com@googlemail.com>
2017-08-12 18:57:27 +0200

    Optimize media-single layout

--- Media/Theme/Backend/media-single.tpl.php
+++ Media/Theme/Backend/media-single.tpl.php
@@ -33,8 +33,13 @@ echo $this->getData('nav')->render();
                         <tr><td>Created at<td><?= htmlspecialchars($media->getCreatedAt()->format('Y-m-d'), ENT_COMPAT, 'utf-8'); ?>
                         <tr><td>Created by<td><?= htmlspecialchars($media->getCreatedBy()->getName1(), ENT_COMPAT, 'utf-8'); ?>
                         <tr><td>Description<td><?= htmlspecialchars($media->getDescription(), ENT_COMPAT, 'utf-8'); ?>
-                        <tr><td colspan="2">Content
                 </table>
+            </div>
+        </section>
+    </div>
+    <div class="col-xs-12">
+        <section class="box wf-100">
+            <div class="inner">
                 <?php if(\phpOMS\System\File\FileUtils::getExtensionType($media->getExtension()) === \phpOMS\System\File\ExtensionType::IMAGE) : ?>
                     <div class="h-overflow"><img src="<?= htmlspecialchars($this->request->getUri()->getBase() . $media->getPath(), ENT_COMPAT, 'utf-8'); ?>"></div>
                 <?php elseif($media->getExtension() === 'collection') : ?>
@@ -49,7 +54,7 @@ echo $this->getData('nav')->render();
                     $output = file_get_contents(__DIR__ . '/../../../../' . $media->getPath());
                     $output = str_replace(["\r\n", "\r"], "\n", $output);
                     $output = explode("\n", $output);
-                    foreach($output as $line) : ?><span><?= $line; ?></span><?php endforeach; ?>
+                    foreach($output as $line) : ?><span><?= htmlspecialchars($line, ENT_COMPAT, 'utf-8'); ?></span><?php endforeach; ?>
                     </pre>
                 <?php endif; ?>
             </div>
