f3a8d8bec1b520b1fb46fb5df551c39884b0ce5f
Dennis Eichhorn <spl1nes.com@googlemail.com>
2017-08-13 21:55:10 +0200

    Improved action draft

--- Profile/Theme/Backend/Components/AccountGroupSelector/base.tpl.php
+++ Profile/Theme/Backend/Components/AccountGroupSelector/base.tpl.php
@@ -13,7 +13,7 @@
 <input type="text" list="<?= htmlspecialchars($this->getId(), ENT_COMPAT, 'utf-8'); ?>-datalist" id="<?= htmlspecialchars($this->getId(), ENT_COMPAT, 'utf-8'); ?>" name="receiver" placeholder="&#xf007; Guest" data-action='[
     {
         "key": 1, "listener": "keyup", "action": [
-            {"key": 1, "type": "validate.keypress", "pressed": "!enter"},
+            {"key": 1, "type": "validate.keypress", "pressed": "!13"},
             {"key": 2, "type": "utils.timer", "id": "<?= htmlspecialchars($this->getId(), ENT_COMPAT, 'utf-8'); ?>", "delay": 500, "resets": true},
             {"key": 3, "type": "dom.datalist.clear", "id": "<?= htmlspecialchars($this->getId(), ENT_COMPAT, 'utf-8'); ?>-datalist"},
             {"key": 4, "type": "message.request", "uri": "{/base}/{/lang}/api/admin/find/account?search={#<?= htmlspecialchars($this->getId(), ENT_COMPAT, 'utf-8'); ?>}", "method": "GET", "request_type": "json"},
@@ -21,25 +21,14 @@
         ]
     },
     {
-        "key": 2, "listener": "keyup", "action" : [
-            {"key": 1, "type": "validate.keypress", "pressed": "enter"},
+        "key": 2, "listener": "keydown", "action" : [
+            {"key": 1, "type": "validate.keypress", "pressed": "13"},
             {"key": 2, "type": "message.request", "uri": "{/base}/{/lang}/api/admin/find/account?search={#<?= htmlspecialchars($this->getId(), ENT_COMPAT, 'utf-8'); ?>}", "method": "GET", "request_type": "json"},
-            {"key": 3, "type": "dom.set", "id": "<?= htmlspecialchars($this->getId(), ENT_COMPAT, 'utf-8'); ?>-idlist" "data": ""}
+            {"key": 3, "type": "dom.setvalue", "overwrite": false, "id": "<?= htmlspecialchars($this->getId(), ENT_COMPAT, 'utf-8'); ?>-idlist", "data-path": "", "data": ""},
+            {"key": 4, "type": "dom.setvalue", "overwrite": false, "id": "<?= htmlspecialchars($this->getId(), ENT_COMPAT, 'utf-8'); ?>-taglist", "data-path": "", "data": ""}
         ]
     }
 ]' required><!-- todo: handle keyup-enter -->
 <datalist id="<?= htmlspecialchars($this->getId(), ENT_COMPAT, 'utf-8'); ?>-datalist"></datalist>
-<input type="text" id="<?= htmlspecialchars($this->getId(), ENT_COMPAT, 'utf-8'); ?>-list"></span>
-<!--
-<ul class="select" data-action='[
-    {
-        "listener": "click", "selector": "ul.select li" "action": [
-            {"key": 1, "type": "dom.add", "id": "taglist", "schema": "<span data-value=\"{$value}\">{$text}</span>", "position": "end"},
-            {"key": 2, "type": "dom.bind", "id": "asfdsaf"}
-        ]
-    }
-]'>
-    <li data-value="my value">Text
-</ul>
--->
-<div id="tag-list"></div>
+<input type="hidden" id="<?= htmlspecialchars($this->getId(), ENT_COMPAT, 'utf-8'); ?>-idlist"></span>
+<div id="<?= htmlspecialchars($this->getId(), ENT_COMPAT, 'utf-8'); ?>-taglist"></div>
