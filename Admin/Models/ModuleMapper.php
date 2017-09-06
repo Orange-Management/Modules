a39e0c282538494ff4e4f295823529ddf116bfcb
Dennis Eichhorn <spl1nes.com@googlemail.com>
2017-09-06 15:30:03 +0200

    Added module mapper and fixed group

---
+++ Admin/Models/ModuleMapper.php
@@ -0,0 +1,76 @@
+<?php
+/**
+ * Orange Management
+ *
+ * PHP Version 7.1
+ *
+ * @category   TBD
+ * @package    TBD
+ * @copyright  Dennis Eichhorn
+ * @license    OMS License 1.0
+ * @version    1.0.0
+ * @link       http://orange-management.com
+ */
+declare(strict_types=1);
+namespace Modules\Admin\Models;
+
+use phpOMS\DataStorage\Database\DataMapperAbstract;
+use phpOMS\DataStorage\Database\RelationType;
+
+class ModuleMapper extends DataMapperAbstract
+{
+
+    /**
+     * Columns.
+     *
+     * @var array
+     * @since 1.0.0
+     */
+    protected static $columns = [
+        'module_id'      => ['name' => 'module_id', 'type' => 'int', 'internal' => 'id'],
+        'module_name'    => ['name' => 'module_name', 'type' => 'string', 'internal' => 'name'],
+        'module_status'    => ['name' => 'module_status', 'type' => 'int', 'internal' => 'status'],
+        'module_desc'    => ['name' => 'module_desc', 'type' => 'string', 'internal' => 'description'],
+        'module_created' => ['name' => 'module_created', 'type' => 'DateTime', 'internal' => 'createdAt'],
+    ];
+
+    /**
+     * Primary table.
+     *
+     * @var string
+     * @since 1.0.0
+     */
+    protected static $table = 'group';
+
+    /**
+     * Primary field name.
+     *
+     * @var string
+     * @since 1.0.0
+     */
+    protected static $primaryField = 'group_id';
+
+    /**
+     * Created at column
+     *
+     * @var string
+     * @since 1.0.0
+     */
+    protected static $createdAt = 'group_created';
+
+    /**
+     * Get object.
+     *
+     * @param mixed $primaryKey Key
+     * @param int   $relations  Load relations
+     * @param mixed $fill       Object to fill
+     *
+     * @return Group
+     *
+     * @since  1.0.0
+     */
+    public static function get($primaryKey, int $relations = RelationType::ALL, $fill = null)
+    {
+        return parent::get($primaryKey, $relations, $fill);
+    }
+}
