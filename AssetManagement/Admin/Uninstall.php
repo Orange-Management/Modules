36dcef80c952c1f6fb9de35a4c25e6333b86b63d
Dennis Eichhorn <spl1nes.com@googlemail.com>
2017-10-04 20:43:06 +0200

    fix #18

---
+++ AssetManagement/Admin/Uninstall.php
@@ -0,0 +1,39 @@
+<?php
+/**
+ * Orange Management
+ *
+ * PHP Version 7.1
+ *
+ * @category   TBD
+ * @package    TBD
+ * @copyright  Dennis Eichhorn
+ * @license    OMS License 1.0
+ * @version    1.0.0
+ * @link       http://orange-management.com
+ */
+declare(strict_types=1);
+namespace Modules\AssetManagement\Admin;
+
+use phpOMS\DataStorage\Database\DatabasePool;
+use phpOMS\Module\UninstallAbstract;
+
+/**
+ * Navigation class.
+ *
+ * @category   Modules
+ * @package    Modules\Admin
+ * @license    OMS License 1.0
+ * @link       http://orange-management.com
+ * @since      1.0.0
+ */
+class Uninstall extends UninstallAbstract
+{
+
+    /**
+     * {@inheritdoc}
+     */
+    public static function uninstall(DatabasePool $dbPool, InfoManager $info)
+    {
+        parent::uninstall($dbPool, $info);
+    }
+}
