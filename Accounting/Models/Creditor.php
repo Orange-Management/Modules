6ea70ac990e66573859d2b7b018592c9bedc268b
Dennis Eichhorn <spl1nes.com@googlemail.com>
2017-11-25 16:06:53 +0100

    Implement more tests

--- Accounting/Models/Creditor.php
+++ Accounting/Models/Creditor.php
@@ -14,8 +14,6 @@
 declare(strict_types = 1);
 namespace Modules\Accounting\Models;
 
-use phpOMS\Utils\IO\ExchangeInterface;
-
 /**
  * Creditor class.
  *
@@ -25,7 +23,7 @@ use phpOMS\Utils\IO\ExchangeInterface;
  * @link       http://website.orange-management.de
  * @since      1.0.0
  */
-abstract class Creditor implements ExchangeInterface
+class Creditor
 {
 
     /**
@@ -34,62 +32,6 @@ abstract class Creditor implements ExchangeInterface
      * @since  1.0.0
      */
     public function __construct()
-    {
-    }
-
-    /**
-     * {@inheritdoc}
-     */
-    public function exportJson($path)
-    {
-    }
-
-    /**
-     * {@inheritdoc}
-     */
-    public function importJson($path)
-    {
-    }
-
-    /**
-     * {@inheritdoc}
-     */
-    public function exportCsv($path)
-    {
-    }
-
-    /**
-     * {@inheritdoc}
-     */
-    public function importCsv($path)
-    {
-    }
-
-    /**
-     * {@inheritdoc}
-     */
-    public function exportExcel($path)
-    {
-    }
-
-    /**
-     * {@inheritdoc}
-     */
-    public function importExcel($path)
-    {
-    }
-
-    /**
-     * {@inheritdoc}
-     */
-    public function exportPdf($path)
-    {
-    }
-
-    /**
-     * {@inheritdoc}
-     */
-    public function importPdf($path)
     {
     }
 }
