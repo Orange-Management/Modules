45d6ba09368c74548efd08e151c15951253661ed
Dennis Eichhorn <spl1nes.com@googlemail.com>
2017-12-17 14:28:55 +0100

    Put content in container

--- Help/Theme/Backend/help-general.tpl.php
+++ Help/Theme/Backend/help-general.tpl.php
@@ -1,1 +1,11 @@
-<?= $this->getData('markdown'); ?>
+<div class="row">
+    <div class="col-xs-12">
+        <section class="box wf-100">
+            <div class="inner">
+                <article>
+                    <?= $this->getData('markdown'); ?>
+                </article>
+            </div>
+        </section>
+    </div>
+</div>
---
+++ Help/Theme/Backend/help-module.tpl.php
@@ -0,0 +1,11 @@
+<div class="row">
+    <div class="col-xs-12">
+        <section class="box wf-100">
+            <div class="inner">
+                <article>
+                    <?= $this->getData('markdown'); ?>
+                </article>
+            </div>
+        </section>
+    </div>
+</div>
