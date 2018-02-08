dfb68f4db6f09e8b3a14bbd3cc4af1a64f2e9361
Dennis Eichhorn <spl1nes.com@googlemail.com>
2018-02-08 20:06:12 +0100

    Implement generic module tests

--- tests/Z/ModuleTest.php
+++ tests/Z/ModuleTest.php
@@ -11,10 +11,11 @@
  * @link       http://website.orange-management.de
  */
 
-namespace Modules\tests;
+namespace Modules\tests\Z;
 
 use phpOMS\ApplicationAbstract;
 use phpOMS\DataStorage\Database\DatabasePool;
+use phpOMS\DataStorage\Database\Query\Builder;
 use phpOMS\Dispatcher\Dispatcher;
 use phpOMS\Module\ModuleFactory;
 use phpOMS\Module\ModuleManager;
@@ -37,24 +38,39 @@ class ModuleTest extends \PHPUnit\Framework\TestCase
         $this->app->dispatcher = new Dispatcher($this->app);
     }
 
+    /**
+     * @group final
+     */
     public function testMembers()
     {
-        $moduleManager = new ModuleManager($this->app, __DIR__ . '/../../Modules');
+        $moduleManager = new ModuleManager($this->app, __DIR__ . '/../../../Modules');
         $allModules    = $moduleManager->getInstalledModules(false);
         $sampleInfo    = json_decode(file_get_contents(__DIR__ . '/info.json'), true); 
-        $totalRoutes   = include __DIR__ . '/../../Web/Backend/Routes.php';
+        $totalRoutes   = include __DIR__ . '/../../../Web/Backend/Routes.php';
 
         foreach ($allModules as $name => $module) {
             $module = $moduleManager->get($name);
 
             if (!($module instanceof NullModule)) {
                 self::assertEquals($name, $module::MODULE_NAME);
-                self::assertEquals(realpath(__DIR__ . '/../../Modules/' . $module::MODULE_NAME), $module::MODULE_PATH);
+                self::assertEquals(realpath(__DIR__ . '/../../../Modules/' . $module::MODULE_NAME), $module::MODULE_PATH);
                 $version = Version::compare($module::MODULE_VERSION, '1.0.0');
                 self::assertGreaterThanOrEqual(0, $version);
 
-                // todo: test providings
-                // todo: test nav setup
+                if (isset($allModules['Navigation']) 
+                    && file_exists($module::MODULE_PATH . '/Admin/Install/Navigation.install.json')
+                ) {
+                    self::assertTrue(
+                        $this->navLinksTest(
+                            $this->app->dbPool->get(), 
+                            json_decode(
+                                file_get_contents($module::MODULE_PATH . '/Admin/Install/Navigation.install.json'),
+                                true
+                            ),
+                            $name
+                        )
+                    );
+                }
 
                 if (file_exists($module::MODULE_PATH . '/Admin/Routes/Web/Backend.php')) {
                     $moduleRoutes = include $module::MODULE_PATH . '/Admin/Routes/Web/Backend.php';
@@ -67,6 +83,25 @@ class ModuleTest extends \PHPUnit\Framework\TestCase
                 self::assertTrue($this->dependencyTest($info, $allModules));
             }
         }
+    }
+
+    private function navLinksTest($db, array $links, string $module) : bool
+    {
+        $query = new Builder($db);
+        $query->select('nav_id')->from('oms_nav')->where('nav_from', '=', $module);
+
+        $result = $query->execute()->fetchAll(\PDO::FETCH_COLUMN);
+        $it     = new \RecursiveIteratorIterator(new \RecursiveArrayIterator($links));
+
+        foreach ($it as $link) {
+            if (is_array($link)
+                && !in_array($link['id'], $result)
+            ) {
+                return false;
+            }
+        }
+
+        return true;
     }
 
     private function dependencyTest(array $info, array $modules) : bool
