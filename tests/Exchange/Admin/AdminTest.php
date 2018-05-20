b282c2edd7c68e8a339e9826b2fcff39c6f06ceb
Dennis Eichhorn <spl1nes.com@googlemail.com>
2018-05-20 09:58:59 +0200

    Added new module for import/export

---
+++ tests/Exchange/Admin/AdminTest.php
@@ -0,0 +1,43 @@
+<?php
+/**
+ * Orange Management
+ *
+ * PHP Version 7.2
+ *
+ * @package    TBD
+ * @copyright  Dennis Eichhorn
+ * @license    OMS License 1.0
+ * @version    1.0.0
+ * @link       http://website.orange-management.de
+ */
+
+namespace Modules\tests\Exchange\Admin;
+
+use Modules\Exchange\Admin\Installer;
+use Modules\Exchange\Admin\Uninstall;
+use phpOMS\ApplicationAbstract;
+use phpOMS\DataStorage\Database\DatabasePool;
+use phpOMS\Module\ModuleManager;
+
+class AdminTest extends \PHPUnit\Framework\TestCase
+{
+
+    /**
+     * @group admin
+     * @slowThreshold 5000
+     */
+    public function testModuleIntegration()
+    {
+        $app         = new class extends ApplicationAbstract {};
+        $app->dbPool = $GLOBALS['dbpool'];
+
+        $moduleManager = new ModuleManager($app, __DIR__ . '/../../../../Modules');
+        $moduleManager->install('Exchange');
+
+        $moduleManager->deactivate('Exchange');
+        self::assertFalse($moduleManager->isActive('Exchange'));
+
+        $moduleManager->activate('Exchange');
+        self::assertTrue($moduleManager->isActive('Exchange'));
+    }
+}
