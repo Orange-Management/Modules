829c59fbbf67a78156286e2b787aebb641de67e5
Dennis Eichhorn <spl1nes.com@googlemail.com>
2018-12-01 22:25:38 +0100

    Update hook handling

--- Auditor/Admin/Hooks/Web/Api.php
+++ Auditor/Admin/Hooks/Web/Api.php
@@ -1,7 +1,13 @@
 <?php
 
 return [
-    'PRE:Module:.*?\-create' => '\Modules\Auditor\Controller\ApiController:apiLogCreate',
-    'PRE:Module:.*?\-update' => '\Modules\Auditor\Controller\ApiController:apiLogUpdate',
-    'PRE:Module:.*?\-delete' => '\Modules\Auditor\Controller\ApiController:apiLogDelete',
+    'PRE:Module:.*?\-create' => [
+        'callback' => ['\Modules\Auditor\Controller\ApiController:apiLogCreate'],
+    ],
+    'PRE:Module:.*?\-update' => [
+        'callback' => ['\Modules\Auditor\Controller\ApiController:apiLogUpdate'],
+    ],
+    'PRE:Module:.*?\-delete' => [
+        'callback' => ['\Modules\Auditor\Controller\ApiController:apiLogDelete'],
+    ],
 ];
--- Auditor/Controller/ApiController.php
+++ Auditor/Controller/ApiController.php
@@ -20,9 +20,48 @@ use phpOMS\Message\ResponseAbstract;
 use phpOMS\Message\NotificationLevel;
 use phpOMS\Module\ModuleAbstract;
 use phpOMS\Views\View;
+use phpOMS\Account\Account;
 use phpOMS\Account\PermissionType;
 use phpOMS\DataStorage\Database\RelationType;
 
 final class ApiController extends Controller
 {
+    public function apiLogCreate(
+        Account $account,
+        $old,
+        $new,
+        int $type = 0,
+        int $subtype = 0,
+        string $module = null,
+        string $content = null
+    ) : void
+    {
+
+    }
+
+    public function apiLogUpdate(
+        Account $account,
+        $old,
+        $new,
+        int $type = 0,
+        int $subtype = 0,
+        string $module = null,
+        string $content = null
+    ) : void
+    {
+
+    }
+
+    public function apiLogDelete(
+        Account $account,
+        $old,
+        $new,
+        int $type = 0,
+        int $subtype = 0,
+        string $module = null,
+        string $content = null
+    ) : void
+    {
+
+    }
 }
