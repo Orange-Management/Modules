c1af4d27b6c36edf8f93624fb636e579cfaf68ab
Dennis Eichhorn <spl1nes.com@googlemail.com>
2018-12-19 17:54:35 +0100

    Create GSDCustomerMapper.php

---
+++ Exchange/Interfaces/GSD/Model/GSDCustomerMapper.php
@@ -0,0 +1,72 @@
+<?php
+/**
+ * Orange Management
+ *
+ * PHP Version 7.2
+ *
+ * @package    Interfaces\GSD\Models
+ * @copyright  Dennis Eichhorn
+ * @license    OMS License 1.0
+ * @version    1.0.0
+ * @link       http://website.orange-management.de
+ */
+declare(strict_types=1);
+
+namespace Interfaces\GSD\Models;
+
+use phpOMS\DataStorage\Database\DataMapperAbstract;
+
+/**
+ * Mapper class.
+ *
+ * @package    Interfaces\GSD\Models
+ * @license    OMS License 1.0
+ * @link       http://website.orange-management.de
+ * @since      1.0.0
+ */
+final class GSDCustomerMapper extends DataMapperAbstract
+{
+    /**
+     * Columns.
+     *
+     * @var array<string, array<string, bool|string>>
+     * @since 1.0.0
+     */
+    protected static $columns = [
+        'row_id'               => ['name' => 'row_id', 'type' => 'int', 'internal' => 'id'],
+        'row_create_time'      => ['name' => 'row_create_time', 'type' => 'DateTime', 'internal' => 'createdAt'],
+        'row_create_user'      => ['name' => 'row_create_user', 'type' => 'int', 'internal' => 'createdBy'],
+        'Kundennummer'         => ['name' => 'Kundennummer', 'type' => 'string', 'internal' => 'number'],
+    ];
+    
+    protected static $ownsOne = [
+        'mainAddress' => [
+            'mapper' => GSDAddress::class,
+            'src'    => 'AddressId',
+        ],
+    ];
+
+    /**
+     * Primary table.
+     *
+     * @var string
+     * @since 1.0.0
+     */
+    protected static $table = 'Kunden';
+
+    /**
+     * Created at.
+     *
+     * @var string
+     * @since 1.0.0
+     */
+    protected static $createdAt = 'row_create_time';
+
+    /**
+     * Primary field name.
+     *
+     * @var string
+     * @since 1.0.0
+     */
+    protected static $primaryField = 'row_id';
+}
