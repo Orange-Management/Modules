f5f4850472406bd1f0dc53711916ecd1ac3fe164
Dennis Eichhorn <spl1nes.com@googlemail.com>
2018-12-23 15:18:00 +0100

    Move schema to json

--- Monitoring/Admin/Installer.php
+++ Monitoring/Admin/Installer.php
@@ -14,9 +14,6 @@ declare(strict_types=1);
 
 namespace Modules\Monitoring\Admin;
 
-use phpOMS\DataStorage\Database\DatabasePool;
-use phpOMS\DataStorage\Database\DatabaseType;
-use phpOMS\Module\InfoManager;
 use phpOMS\Module\InstallerAbstract;
 
 /**
@@ -29,36 +26,4 @@ use phpOMS\Module\InstallerAbstract;
  */
 class Installer extends InstallerAbstract
 {
-
-    /**
-     * {@inheritdoc}
-     */
-    public static function install(DatabasePool $dbPool, InfoManager $info) : void
-    {
-        parent::install($dbPool, $info);
-
-         switch ($dbPool->get()->getType()) {
-            case DatabaseType::MYSQL:
-                $dbPool->get()->con->beginTransaction();
-
-                $dbPool->get()->con->prepare(
-                    'CREATE TABLE if NOT EXISTS `' . $dbPool->get()->prefix . 'monitoring_file` (
-                            `monitoring_file_id` int(11) NOT NULL AUTO_INCREMENT,
-                            `monitoring_file_path` text DEFAULT NULL,
-                            `monitoring_file_modified` datetime NOT NULL,
-                            `monitoring_file_deprecated` tinyint(1) NOT NULL,
-                            `monitoring_file_dangerous` tinyint(1) NOT NULL,
-                            `monitoring_file_unicode` tinyint(1) NOT NULL,
-                            `monitoring_file_version` tinyint(1) NOT NULL,
-                            `monitoring_file_hash` tinyint(1) NOT NULL,
-                            `monitoring_file_status` tinyint(1) NOT NULL,
-                            `monitoring_file_inspected` datetime NOT NULL,
-                            PRIMARY KEY (`monitoring_file_id`)
-                        )ENGINE=InnoDB DEFAULT CHARSET=utf8 AUTO_INCREMENT=1;'
-                )->execute();
-
-                $dbPool->get()->con->commit();
-                break;
-        }
-    }
 }
