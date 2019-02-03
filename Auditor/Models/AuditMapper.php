6cfa292362f9d180459adcb6c529ad80cb905238
Dennis Eichhorn <spl1nes.com@googlemail.com>
2019-02-03 17:54:12 +0100

    fix indention

--- Auditor/Models/AuditMapper.php
+++ Auditor/Models/AuditMapper.php
@@ -35,17 +35,17 @@ final class AuditMapper extends DataMapperAbstract
      * @since 1.0.0
      */
     protected static $columns = [
-        'auditor_audit_id'         => ['name' => 'auditor_audit_id', 'type' => 'int', 'internal' => 'id'],
-        'auditor_audit_created_by' => ['name' => 'auditor_audit_created_by', 'type' => 'int', 'internal' => 'createdBy'],
+        'auditor_audit_id'         => ['name' => 'auditor_audit_id',         'type' => 'int',      'internal' => 'id'],
+        'auditor_audit_created_by' => ['name' => 'auditor_audit_created_by', 'type' => 'int',      'internal' => 'createdBy'],
         'auditor_audit_created_at' => ['name' => 'auditor_audit_created_at', 'type' => 'DateTime', 'internal' => 'createdAt'],
-        'auditor_audit_ip'         => ['name' => 'auditor_audit_ip', 'type' => 'int', 'internal' => 'ip'],
-        'auditor_audit_module'     => ['name' => 'auditor_audit_module', 'type' => 'string', 'internal' => 'module'],
-        'auditor_audit_ref'        => ['name' => 'auditor_audit_ref', 'type' => 'string', 'internal' => 'ref'],
-        'auditor_audit_type'       => ['name' => 'auditor_audit_type', 'type' => 'int', 'internal' => 'type'],
-        'auditor_audit_subtype'    => ['name' => 'auditor_audit_subtype', 'type' => 'int', 'internal' => 'subtype'],
-        'auditor_audit_content'    => ['name' => 'auditor_audit_content', 'type' => 'string', 'internal' => 'content'],
-        'auditor_audit_old'        => ['name' => 'auditor_audit_old', 'type' => 'string', 'internal' => 'old'],
-        'auditor_audit_new'        => ['name' => 'auditor_audit_new', 'type' => 'string', 'internal' => 'new'],
+        'auditor_audit_ip'         => ['name' => 'auditor_audit_ip',         'type' => 'int',      'internal' => 'ip'],
+        'auditor_audit_module'     => ['name' => 'auditor_audit_module',     'type' => 'string',   'internal' => 'module'],
+        'auditor_audit_ref'        => ['name' => 'auditor_audit_ref',        'type' => 'string',   'internal' => 'ref'],
+        'auditor_audit_type'       => ['name' => 'auditor_audit_type',       'type' => 'int',      'internal' => 'type'],
+        'auditor_audit_subtype'    => ['name' => 'auditor_audit_subtype',    'type' => 'int',      'internal' => 'subtype'],
+        'auditor_audit_content'    => ['name' => 'auditor_audit_content',    'type' => 'string',   'internal' => 'content'],
+        'auditor_audit_old'        => ['name' => 'auditor_audit_old',        'type' => 'string',   'internal' => 'old'],
+        'auditor_audit_new'        => ['name' => 'auditor_audit_new',        'type' => 'string',   'internal' => 'new'],
     ];
 
     protected static $belongsTo = [
