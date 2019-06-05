17d6e7374bad731e9552469eede079e51a70163d
Dennis Eichhorn <spl1nes.com@googlemail.com>
2019-06-05 21:50:43 +0200

    Add test/install for search

---
+++ tests/Search/Admin/AdminTest.php
@@ -0,0 +1,25 @@
+<?php declare(strict_types=1);
+/**
+ * Orange Management
+ *
+ * PHP Version 7.2
+ *
+ * @package    tests
+ * @copyright  Dennis Eichhorn
+ * @license    OMS License 1.0
+ * @version    1.0.0
+ * @link       http://website.orange-management.de
+ */
+
+namespace Modules\tests\Search\Admin;
+
+/**
+ * @internal
+ */
+class AdminTest extends \PHPUnit\Framework\TestCase
+{
+    protected const MODULE_NAME = 'Search';
+    protected const URI_LOAD = '';
+
+    use \Modules\tests\ModuleTestTrait;
+}
