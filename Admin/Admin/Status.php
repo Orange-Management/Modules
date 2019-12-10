bd3ba8e07b9fc387b62fcdbced8835c4e8d2d663
Dennis Eichhorn <spl1nes.com@googlemail.com>
2019-12-10 21:33:05 +0100

    make final

--- Admin/Admin/Status.php
+++ Admin/Admin/Status.php
@@ -24,7 +24,7 @@ use phpOMS\Module\StatusAbstract;
  * @link    https://orange-management.org
  * @since   1.0.0
  */
-class Status extends StatusAbstract
+final class Status extends StatusAbstract
 {
 
 }
