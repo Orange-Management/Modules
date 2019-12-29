5abb11360b4a49bedfe82bdc819b1fcc866e17fb
Dennis Eichhorn <spl1nes.com@googlemail.com>
2019-12-29 18:01:31 +0100

    Restructure news controller test

---
+++ tests/News/Controller/ApiControllerTest.php
@@ -0,0 +1,85 @@
+<?php
+/**
+ * Orange Management
+ *
+ * PHP Version 7.4
+ *
+ * @package   tests
+ * @copyright Dennis Eichhorn
+ * @license   OMS License 1.0
+ * @version   1.0.0
+ * @link      https://orange-management.org
+ */
+declare(strict_types=1);
+
+namespace Modules\tests\News\Controller;
+
+require_once __DIR__ . '/../../Autoloader.php';
+
+use Model\CoreSettings;
+
+use Modules\Admin\Models\AccountPermission;
+use Modules\tests\Admin\Controller\Api\ApiControllerNewsArticleTrait;
+use phpOMS\Account\Account;
+use phpOMS\Account\AccountManager;
+use phpOMS\Account\PermissionType;
+use phpOMS\ApplicationAbstract;
+use phpOMS\Dispatcher\Dispatcher;
+use phpOMS\Event\EventManager;
+use phpOMS\Module\ModuleManager;
+use phpOMS\Router\WebRouter;
+use phpOMS\Utils\TestUtils;
+
+/**
+ * @testdox Modules\tests\News\Controller\ApiControllerTest: News api controller
+ * 
+ * @internal
+ */
+class ControllerTest extends \PHPUnit\Framework\TestCase
+{
+    protected $app    = null;
+    protected $module = null;
+
+    protected function setUp() : void
+    {
+        $this->app = new class() extends ApplicationAbstract
+        {
+            protected string $appName = 'Api';
+        };
+
+        $this->app->dbPool         = $GLOBALS['dbpool'];
+        $this->app->orgId          = 1;
+        $this->app->appName        = 'Backend';
+        $this->app->accountManager = new AccountManager($GLOBALS['session']);
+        $this->app->appSettings    = new CoreSettings($this->app->dbPool->get());
+        $this->app->moduleManager  = new ModuleManager($this->app, __DIR__ . '/../../../Modules');
+        $this->app->dispatcher     = new Dispatcher($this->app);
+        $this->app->eventManager   = new EventManager($this->app->dispatcher);
+        $this->app->eventManager->importFromFile(__DIR__ . '/../../../Web/Api/Hooks.php');
+
+        $account = new Account();
+        TestUtils::setMember($account, 'id', 1);
+
+        $permission = new AccountPermission();
+        $permission->setUnit(1);
+        $permission->setApp('backend');
+        $permission->setPermission(
+            PermissionType::READ
+            | PermissionType::CREATE
+            | PermissionType::MODIFY
+            | PermissionType::DELETE
+            | PermissionType::PERMISSION
+        );
+
+        $account->addPermission($permission);
+
+        $this->app->accountManager->add($account);
+        $this->app->router = new WebRouter();
+
+        $this->module = $this->app->moduleManager->get('News');
+
+        TestUtils::setMember($this->module, 'app', $this->app);
+    }
+
+    use ApiControllerNewsArticleTrait;
+}
