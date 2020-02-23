c5c72e00478b7b262ff612e92eb0d2ebf0a191aa
Dennis Eichhorn <spl1nes.com@googlemail.com>
2020-02-23 22:57:52 +0100

    template improvements

--- Help/Theme/Backend/help-module.tpl.php
+++ Help/Theme/Backend/help-module.tpl.php
@@ -9,7 +9,7 @@
 
     <div class="col-xs-12 col-md-4 col-lg-3">
         <div class="box wf-100">
-            <a class="button" href="<?= \phpOMS\Uri\UriFactory::build('{/lang}/backend/help/module/single?id={?id}'); ?>">Module</a>
+            <a class="button" href="<?= \phpOMS\Uri\UriFactory::build('{/lang}/backend/help/module/single?id={?id}'); ?>"><?= $this->getHtml('Module'); ?></a>
         </div>
 
         <section class="box wf-100">
